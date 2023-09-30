18f6348e778bdffb2e5167646d701ec9dab00999
Sem Voigtländer <43578852+userlandkernel@users.noreply.github.com>
2023-09-30 14:36:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,156 @@
-<?php
 
-function GenerateAuthToken(){
-	return bin2hex(mcrypt_create_iv(22, MCRYPT_DEV_URANDOM));
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Admin Panel Log in</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.6 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <style>
+    pre {
+        color: white !important;
+        font-size: 12px;
+    }
+        body {
+    background: #222D32;
+    font-family: 'Roboto', sans-serif;
 }
 
-function Login($pubkeyFile, $signature) {	
-    $publicKey = file_get_contents($pubkeyFile);
-    $verifyResult = openssl_verify($_SESSION['AuthToken'], base64_decode($signature), $publicKey, OPENSSL_ALGO_SHA256);
-    return $verifyResult;
-   
+.login-box {
+    margin-top: 75px;
+    height: auto;
+    background: #1A2226;
+    text-align: center;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 }
 
-// Create a new session
- session_start();
-    
-// Check if user needs authentication
-if (!isset($_SESSION['active'])) {
-    
-    // Check if the user has a message to sign    
-    if (!isset($_SESSION['AuthToken'])) {
-        $_SESSION['AuthToken'] = GenerateAuthToken(); // If not generate it
-    }
-    // Display the token to sign for authentication and the login form
-    echo "<strong>Please sign this message: </strong><pre>".$_SESSION['AuthToken']."</pre><br/>";
-    echo "<form action='#' method='post'><strong>Enter signature: </strong><input name='signature'><br/><input type='submit'></form>";
- 
-    // Check if the user posted the signature and has an auth token
-     if (isset($_POST['signature']) && isset($_SESSION['AuthToken'])) {
-            
-        // Perform the signature verification (login)
-        if (Login("public.key", $_POST['signature'])){
-            $_SESSION['active'] = 1;
-            header("Refresh:0");
-        }
-        else {
-            echo "Invalid signature, please try again.";
-        }
-    }
-    
+.login-key {
+    height: 100px;
+    font-size: 80px;
+    line-height: 100px;
+    background: -webkit-linear-gradient(#27EF9F, #0DB8DE);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 }
-// If the session is active
-else {
-    // Verify the login is succesful
-    if($_SESSION['active'] == 1) {
-        echo "Logged in!";
-        echo "<a href='logout.php'>Log out</a>";
-    }
-    
+
+.login-title {
+    margin-top: 15px;
+    text-align: center;
+    font-size: 30px;
+    letter-spacing: 2px;
+    margin-top: 15px;
+    font-weight: bold;
+    color: #ECF0F5;
 }
-    
-?>
+
+.login-form {
+    margin-top: 25px;
+    text-align: left;
+}
+
+input[type=text] {
+    background-color: #1A2226;
+    border: none;
+    border-bottom: 2px solid #0DB8DE;
+    border-top: 0px;
+    border-radius: 0px;
+    font-weight: bold;
+    outline: 0;
+    margin-bottom: 20px;
+    padding-left: 0px;
+    color: #ECF0F5;
+}
+
+input[type=password] {
+    background-color: #1A2226;
+    border: none;
+    border-bottom: 2px solid #0DB8DE;
+    border-top: 0px;
+    border-radius: 0px;
+    font-weight: bold;
+    outline: 0;
+    padding-left: 0px;
+    margin-bottom: 20px;
+    color: #ECF0F5;
+}
+
+.form-group {
+    margin-bottom: 40px;
+    outline: 0px;
+}
+
+.form-control:focus {
+    border-color: inherit;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    border-bottom: 2px solid #0DB8DE;
+    outline: 0;
+    background-color: #1A2226;
+    color: #ECF0F5;
+}
+
+input:focus {
+    outline: none;
+    box-shadow: 0 0 0;
+}
+
+label {
+    margin-bottom: 0px;
+}
+
+.form-control-label {
+    font-size: 10px;
+    color: #6C6C6C;
+    font-weight: bold;
+    letter-spacing: 1px;
+}
+
+.btn-outline-primary {
+    border-color: #0DB8DE;
+    color: #0DB8DE;
+    border-radius: 0px;
+    font-weight: bold;
+    letter-spacing: 1px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
+
+.btn-outline-primary:hover {
+    background-color: #0DB8DE;
+    right: 0px;
+}
+
+.login-btm {
+    float: left;
+}
+
+.login-button {
+    padding-right: 0px;
+    text-align: right;
+    margin-bottom: 25px;
+}
+
+.login-text {
+    text-align: left;
+    padding-left: 0px;
+    color: #A2A4A4;
+}
+
+.loginbttm {
+    padding: 0px;
+}
+    </style>
+    </head>
+    <body>
+        <?php
+            include "inc/login.php";
+            include "inc/dashboard.view.php";
+        ?>
+    </body>
+</html>
